912c27b9d2eb62f33a809a434e2ad7be3671c1d1
Alice Marques aquela lá sabe? <86207976+NotArme@users.noreply.github.com>
2026-01-03 23:14:58 -0300

    Started a new bevy guide from the AIBodh blog

---
+++ aibodhs-guide-project/readme.md
@@ -0,0 +1,3 @@
+A barebones bevy game made to learn more about the engine
+
+Project created by following the guide from https://aibodh.com
---
+++ aibodhs-guide-project/src/player.rs
@@ -0,0 +1,18 @@
+use bevy::prelude::*;
+
+const TILE_SIZE: u16 = 64;
+const WALK_FRAMES: usize = 9;
+const MOVE_SPEED: f32 = 140.0;
+const ANIM_DT: f32 = 0.1; //seconds per frame
+
+
+#[derive(Component)]
+struct Player;
+
+#[derive(Component, Debug, Clone, Copy, PartialEq, Eq)]
+enum Facing {
+    Up,
+    Left,
+    Down,
+    Right
+}
