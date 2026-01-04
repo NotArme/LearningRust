1f7dbb88b68cf57af39e118094c443ef98c16045
Alice Marques aquela lá sabe? <86207976+NotArme@users.noreply.github.com>
2026-01-03 21:19:36 -0300

    Done Bevy's starting guide

---
+++ my_bevy_game/src/main.rs
@@ -0,0 +1,54 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(HelloPlugin)
+        .run();
+}
+
+fn hello_world() {
+    println!("yipeeee")
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("rick".to_string())));
+    commands.spawn((Person, Name("roll".to_string())));
+    commands.spawn((Person, Name("another person".to_string())));
+}
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello {}", name.0);
+        }
+    }
+}
+
+fn update_people(mut query: Query<&mut Name, With<Person>>) {
+    for mut name in &mut query {
+        if name.0 == "another person" {
+            name.0 = "jeff".to_string();
+            break;
+        }
+    }
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
+        app.add_systems(Startup, add_people);
+        app.add_systems(Update, ( (update_people, greet_people).chain()));
+    }
+}
