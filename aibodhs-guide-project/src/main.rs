b15a2312334d07eabba1e3ca236f9ec0a5e6718e
Alice Marques aquela lá sabe? <86207976+NotArme@users.noreply.github.com>
2026-01-04 23:05:18 -0300

    Character with atlas based animation added

--- aibodhs-guide-project/src/main.rs
+++ aibodhs-guide-project/src/main.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
 
+use crate::player::PlayerPlugin;
+
 mod player;
 
 
@@ -10,46 +12,23 @@ fn main() {
         file_path: "assets".into(),
         ..default()
         }))
+    .add_plugins(PlayerPlugin)
     .add_systems(Startup, setup_camera)
-    .add_systems(Update, move_player)
     .run();
 }
 
 fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2d);
 
-    commands.spawn((
-        Text2d::new("@"),
-        TextFont {
-            font_size: 24.0,
-            font: default(),
-            ..default()
-        },
-        TextColor(Color::WHITE),
-        Transform::from_translation(Vec3::ZERO),
-        PLayer
-    ));
-}
-
-fn move_player(input: Res<ButtonInput<KeyCode>>, time: Res<Time>, mut player_transform: Single<&mut Transform, With<PLayer>>) {
-    let mut direction = Vec2::ZERO;
-    if input.pressed(KeyCode::ArrowLeft) {
-        direction.x -= 1.0;
-    }
-    if input.pressed(KeyCode::ArrowRight) {
-        direction.x += 1.0;
-    }
-    if input.pressed(KeyCode::ArrowUp) {
-        direction.y += 1.0;
-    }
-    if input.pressed(KeyCode::ArrowDown) {
-        direction.y -= 1.0;
-    }
-
-    if direction != Vec2::ZERO {
-        let speed = 300.0;
-        let velocity = direction.normalize() * speed * time.delta_secs();
-        player_transform.translation.x += velocity.x;
-        player_transform.translation.y += velocity.y;
-    }
+//    commands.spawn((
+//        Text2d::new("@"),
+//        TextFont {
+//            font_size: 24.0,
+//            font: default(),
+//            ..default()
+//        },
+//        TextColor(Color::WHITE),
+//        Transform::from_translation(Vec3::ZERO),
+//        Player
+//    ));
 }
